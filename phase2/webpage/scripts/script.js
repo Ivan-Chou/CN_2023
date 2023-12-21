69354e28f3f9f16aa115ae9b6daf461a7e9f3bb1
BuildTools <unconfigured@null.spigotmc.org>
2023-12-22 02:27:46 +0800

    make the index page prettier

--- phase2/webpage/scripts/script.js
+++ phase2/webpage/scripts/script.js
@@ -1,11 +1,21 @@
 // Event Listeners
 document.getElementById("reg_btn").addEventListener("click", () => {
-    document.getElementById("login_form").style.visibility = "collapse";
-    document.getElementById("reg_form").style.visibility = "visible";
+    for (let i = 0; i < document.getElementsByClassName("login_form").length; i++){
+        document.getElementsByClassName("login_form")[i].style.visibility = "collapse"
+    }
+
+    for (let i = 0; i < document.getElementsByClassName("reg_form").length; i++){
+        document.getElementsByClassName("reg_form")[i].style.visibility = "visible"
+    }
 })
 
 document.getElementById("login_btn").addEventListener("click", () => {
-    document.getElementById("login_form").style.visibility = "visible";
-    document.getElementById("reg_form").style.visibility = "collapse";
+    for (let i = 0; i < document.getElementsByClassName("login_form").length; i++){
+        document.getElementsByClassName("login_form")[i].style.visibility = "visible"
+    }
+
+    for (let i = 0; i < document.getElementsByClassName("reg_form").length; i++){
+        document.getElementsByClassName("reg_form")[i].style.visibility = "collapse"
+    }
 })
 
